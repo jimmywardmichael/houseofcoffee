efd570c492c1c0066d85ae7dc04edc05639d3250
James Ward <jimmywardmichael@gmail.com>
2023-08-14 18:22:00 -0500

    init

---
+++ src/main/java/com/project/springcoffeeshop/SpringcoffeeshopApplication.java
@@ -0,0 +1,13 @@
+package com.project.springcoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoffeeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoffeeshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/springcoffeeshop/SpringcoffeeshopApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.springcoffeeshop;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringcoffeeshopApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
